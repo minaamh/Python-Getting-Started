ac0fd2ce272075eb7a2243728d5e1cb518331534
Mina <mina@openroad.ca>
2018-11-03 16:24:28 -0700

    Functions, Files, Yield, and Lambda : Adding Students to Our App

--- functions.py
+++ functions.py
@@ -1,35 +1,64 @@
 students = []
-
+add_more = True
 
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title
+        students_titlecase.append(student["name"].title())
     return students_titlecase
 
 
 def print_students_titlecase():
     students_titlecase = get_students_titlecase()
-    print(students_titlecase)
+    for student_title in students_titlecase:
+        print(student_title)
 
 
 def add_student(name, student_id=332):
     student = {"name": name, "student_id": student_id}
     students.append(student)
+    save_file(student)
+
+def save_file(student):
+    try:
+        f = open("students.txt","a")
+        f.write(student,"\n")
+        f.close()
+    except Exception as error:
+        print("Could not save to file")
+        print(error)
+
+def read_file():
+    try:
+        f = open("students.txt","r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception as error:
+        print("Could not read from file")
+        print(error)
 
 
-def var_args(name, *args):
-    print(name)
-    print(args)
+while(add_more):
+    student_name = input("Please enter the student name: ")
+    student_ID = input("Please enter the student ID: ")
+    add_student(student_name, student_ID)
+    temp = input("Do you want to add more students? (y/n)")
+    if (temp=='n'):
+        add_more=False
 
 
-def var_args2(name, **kwargs):
-    print (name)
-    print(kwargs["description"], kwargs["feedback"])
+print_students_titlecase()
+#def var_args(name, *args):
+#    print(name)
+#    print(args)
 
-student_list = get_students_titlecase()
 
-add_student(name="Mark", student_id=15)
+#def var_args2(name, **kwargs):
+#    print (name)
+#    print(kwargs["description"], kwargs["feedback"])
 
-var_args("Mark", "Loves Python", None, "Hello", True)
-var_args2("Mark", description = "Loves Python", feedback = None, pluralsight_subscrber=True)
+# add_student(name="Mark", student_id=15)
+#
+# var_args("Mark", "Loves Python", None, "Hello", True)
+# var_args2("Mark", description = "Loves Python", feedback = None, pluralsight_subscrber=True)
